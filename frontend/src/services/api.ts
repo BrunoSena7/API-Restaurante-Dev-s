22e91412db3048f224214c1fc807f343f48dd83c
Pedroleaoo <pedrovitorl291@gmail.com>
2025-12-03 13:47:29 -0300

    ajuste start backend e api url frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,3 +1,4 @@
-const API_URL = "http://localhost:3000/api";
+// frontend/src/services/api.ts
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 
 export default API_URL;
